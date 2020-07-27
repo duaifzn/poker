8a0ab47b35aed047d47434e24aae2e04acccf595
duai <duaifzn@gmail.com>
2020-07-27 08:28:51 +0800

    socket disconnect

--- app.js
+++ app.js
@@ -43,36 +43,11 @@ io.on('connection', socket => {
       }
     })
   })
-})
-io.on('connection', socket => {
-  console.log('socket conection')
-  socket.on('create', room => {
-    console.log('socket id: ' + socket.id + ` join ${room} room`)
-    socket.join(room)
-    let gameService = gameServiceRepository.findById(room)
-    if (!gameService) {
-      gameService = new GameService(room)
-
-      gameServiceRepository.insert(gameService)
-    }
-    gameService.handleMessage(socket.id, 'login')
-    socket.emit('setId', { socketId: socket.id, signIn: true })
-
-    socket.use(packet => {
-      const ROOM = room
-      gameService.handleMessage(socket.id, packet[0], packet[1])
-      let data = gameService.getClientResponse()
-
-      if (data) {
-        io.to(ROOM).emit('state', data);
-      }
-    })
-  })
   socket.on('disconnect', () => {
     console.log('user disconnected');
-
   });
 })
+
 
 
 
